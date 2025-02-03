ffb7705e670deb9d19c3c2a09ef4f11454b5fd16
Eilidh MacNicol <eilidhmacnicol@gmail.com>
2025-02-03 13:08:49 +0000

    fix: mosaic slice arg as int

--- nmriprep/parser.py
+++ nmriprep/parser.py
@@ -69,6 +69,7 @@ def get_parser():
         "--mosaic-slices",
         help="slices indices to include " +
         "in mosaic plot (-1 indicates all slices)",
+        type=int,
         nargs='*'
         )
     parser.add_argument(
