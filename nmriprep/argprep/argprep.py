cffa1c71ef386fde92b4b7074eb95cbc32bdbfcd
eilidhmacnicol <eilidhmacnicol@gmail.com>
2025-04-17 15:07:13 +0100

    ref: allow flatfield specification on a subjectwise basis

--- nmriprep/argprep/argprep.py
+++ nmriprep/argprep/argprep.py
@@ -15,14 +15,6 @@ def main():
     out_dir = src_dir.parent / 'preproc' if not args.output else args.output
     verbose = args.save_intermediate
 
-    # attempt to find flat field info
-    flatfield_correction = {}
-    flatfield_correction['dark'] = find_fields(args.dark_field, out_dir.glob('*darkfield.tif*'))
-    flatfield_correction['flat'] = find_fields(args.flat_field, out_dir.glob('*flatfield.tif*'))
-    if not all(flatfield_correction.values()):
-        print('Skipping flat field correction...')
-        flatfield_correction = None
-
     # identify subjects for pipeline
     all_subject_ids = set(
         [
@@ -47,6 +39,14 @@ def main():
         sub_dir.mkdir(exist_ok=True, parents=True)
         fig_dir = sub_dir / 'figures'
         fig_dir.mkdir(exist_ok=True)
+
+        # attempt to find flat field info
+        flatfield_correction = {}
+        flatfield_correction['dark'] = find_fields(args.dark_field, sub_dir.glob('*darkfield.tif*'))
+        flatfield_correction['flat'] = find_fields(args.flat_field, sub_dir.glob('*flatfield.tif*'))
+        if not all(flatfield_correction.values()):
+            print('Skipping flat field correction...')
+            flatfield_correction = None
 
         # calibrate standards to transform GV to radioactivity
         popt, std_rad, std_gv, std_stem = calibrate_standard(
