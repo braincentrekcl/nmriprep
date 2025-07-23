fix: allow lateralised regions for normalisation

nmriprep/utils.py
@@ -72,7 +72,15 @@ def normalise_by_region(df, region, measure='median'):
     summary_measure = {'mean': np.mean, 'median': np.median}[measure]
     region_df = df.query((f'region == "{region}"'))
     if region_df.duplicated(['sub', 'slide', 'section']).any():
-        raise ValueError('region_df has duplicate keys!')
+        # hemisphere duplicates are allowed, but those are the only exception
+        if not region_df.duplicated(['sub', 'slide', 'section', 'hemi']).any():
+            region_df = (
+                region_df
+                .groupby(['sub', 'slide', 'section'], as_index=False)
+                .agg({'values': lambda arrs: np.concatenate(arrs.values)})
+            )
+        else:
+            raise ValueError('region_df has duplicate keys!')
     region_df[f'{measure}_{region}_values'] = df['values'].apply(summary_measure)
     out = df.merge(
         region_df,